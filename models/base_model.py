0220cb1bcb9df49453e3a7b8bc77a27969b3d794
leonileo <leolittleprogrammer@gmail.com>
2024-11-26 01:12:44 -0800

    Hot fix!

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,22 @@ class BaseModel:
     """Class BaseModel"""
 
     def __init__(self):
+        """Instantiation."""
         tform = "%Y-%m-%dT%H:%M:%S.%f"
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now().strftime(tform)
         self.updated_at = datetime.now().strftime(tform)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Print the class name and it's dict."""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
 
     def save(self):
-        """Updates the public instance attribute updated_at with the current datetime."""
+        """Updates the public instance attribute updated_at
+        with the current datetime."""
         self.updated_at = datetime.now().isoformat()
 
     def to_dict(self):
@@ -27,7 +32,5 @@ class BaseModel:
         dict1["created_at"] = self.created_at
         dict1["updated_at"] = self.updated_at
         dict1["__class__"] = self.__class__.__name__
-        
-        return dict1
 
-m = BaseModel()
+        return dict1
