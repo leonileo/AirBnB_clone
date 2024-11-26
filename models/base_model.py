70f69afa26f5c1b016ff34c2885488811f7921ca
leonileo <leolittleprogrammer@gmail.com>
2024-11-26 00:51:52 -0800

    Hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,11 @@ class BaseModel:
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__."""
-        return self.__dict__
+        dict1 = self.__dict__.copy()
+        dict1["created_at"] = self.created_at
+        dict1["updated_at"] = self.updated_at
+        dict1["__class__"] = self.__class__.__name__
+        
+        return dict1
 
 m = BaseModel()
