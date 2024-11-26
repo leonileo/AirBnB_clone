533feacf95fb64e568aff04d178bf311a5ea7b0f
leonileo <leolittleprogrammer@gmail.com>
2024-11-26 01:26:14 -0800

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ class BaseModel:
         tform = "%Y-%m-%dT%H:%M:%S.%f"
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().strftime(tform)
-        self.updated_at = datetime.now().strftime(tform)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Print the class name and it's dict."""
@@ -24,13 +24,13 @@ class BaseModel:
     def save(self):
         """Updates the public instance attribute updated_at
         with the current datetime."""
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__."""
         dict1 = self.__dict__.copy()
-        dict1["created_at"] = self.created_at
-        dict1["updated_at"] = self.updated_at
+        dict1["created_at"] = self.created_at.isoformat()
+        dict1["updated_at"] = self.updated_at.isoformat()
         dict1["__class__"] = self.__class__.__name__
 
         return dict1
