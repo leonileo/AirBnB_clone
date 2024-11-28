8de515468da0260d0e953cdb1c2a8193436d7345
leonileo <leolittleprogrammer@gmail.com>
2024-11-28 02:09:40 -0800

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -13,12 +13,13 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+        dformat = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        self.__dict__[key] = datetime.fromisoformat(
-                            kwargs[key])
+                        self.__dict__[key] = datetime.strptime(
+                            kwargs[key], dformat)
 
     def __str__(self):
         """Return a string representation of the instance."""
