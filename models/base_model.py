8ed9b78b9658bc4e954758176fd9235ceea74d1f
leonileo <leolittleprogrammer@gmail.com>
2024-11-28 02:36:43 -0800

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,18 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize a new instance with unique id and timestamps."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
         dformat = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(
                         value, dformat)
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the instance."""
