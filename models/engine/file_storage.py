93a839b7e30c9ee8a6081020c788dc088ab6ed9b
leonileo <leolittleprogrammer@gmail.com>
2024-11-29 03:52:25 -0800

    File storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""""File storage"""
+import json
+import os
+
+
+class FileStorage:
+    """
+    Serialize instance to a JSON file and vice versa.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        self.__objects["{}.{}".format(
+            obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file."""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, "w") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects."""
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                obj_dict = json.load(f)
+            for key, value in obj_dict.items():
+                class_name = value["__class__"]
+                from models.base_model import BaseModel
+                cls = eval(class_name)
+                self.__objects[key] = cls(**value)
